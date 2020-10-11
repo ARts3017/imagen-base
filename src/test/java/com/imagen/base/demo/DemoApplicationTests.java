acc55efef3d5c374694308c99884998374c3e179
ARts3017 <arturo.vazquez.1730@gmail.com>
2020-10-10 19:25:46 -0600

    Initial Commit

---
+++ src/test/java/com/imagen/base/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagen.base.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
